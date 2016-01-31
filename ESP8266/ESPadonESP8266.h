f3597024ee551e2c69e8277cacc66bdadbe76aab
David <david.hautbois@free.fr>
2016-01-31 18:49:22 +0100

    handleHTTPRequest startHTTPServer

--- ESP8266/ESPadonESP8266.h
+++ ESP8266/ESPadonESP8266.h
@@ -8,14 +8,22 @@
 #ifndef ESPADONESP8266_H_
 #define ESPADONESP8266_H_
 
-#define A_WIFISTATUS  2
-#define A_WIFIDISCONN 3
-#define A_WIFIBEGIN   4
-#define A_SSID        5
-#define A_BSSID       6
-#define A_MAC         7
-#define A_IP          8
-#define A_SUBNET      9
+//Local actions
+#define A_WIFISTATUS              2
+#define A_WIFIDISCONN             3
+#define A_WIFIBEGIN               4
+#define A_SSID                    5
+#define A_BSSID                   6
+#define A_MAC                     7
+#define A_IP                      8
+#define A_SUBNET                  9
+#define A_STARTHTTPSERVER        10
+
+//Remote actions
+#define A_HTTPREQUEST             2
+
+
+
 
 
 
--- ESP8266/ESPadon___ESP8266.ino
+++ ESP8266/ESPadon___ESP8266.ino
@@ -2,32 +2,39 @@
 #include "ESPadonESP8266.h"
 #include <ESP8266WiFi.h>
 #include <SPI.h>
+#include <ESP8266WebServer.h>
 
 SerialComm s( Serial );
+ESP8266WebServer* httpserver = NULL;
+bool httpserverstarted = false;
+
 
 
 void setup( ) {
-  // put your setup code here, to run once:
+    // put your setup code here, to run once:
 
- Serial.begin( 9600 );
- s.attach( A_WIFISTATUS , wifiStatus );
- s.attach( A_WIFIDISCONN , wifiDisconnect );
- s.attach( A_WIFIBEGIN , wifiBegin );
- s.attach( A_SSID , wifiSSID );
- s.attach( A_BSSID , wifiBSSID );
- s.attach( A_MAC , wifiMAC );
- s.attach( A_IP , wifiIP );
- s.attach( A_SUBNET , wifiSubnet );
-
+    Serial.begin( 9600 );
+    s.attach( A_WIFISTATUS , wifiStatus );
+    s.attach( A_WIFIDISCONN , wifiDisconnect );
+    s.attach( A_WIFIBEGIN , wifiBegin );
+    s.attach( A_SSID , wifiSSID );
+    s.attach( A_BSSID , wifiBSSID );
+    s.attach( A_MAC , wifiMAC );
+    s.attach( A_IP , wifiIP );
+    s.attach( A_SUBNET , wifiSubnet );
+    s.attach( A_STARTHTTPSERVER , startHTTPServer );
 
 
 
 }
 
 void loop () {
-  // put your main code here, to run repeatedly:
-	s.check_reception( );
+    // put your main code here, to run repeatedly:
+    s.check_reception( );
 
+    if ( httpserverstarted ) {
+        httpserver->handleClient();
+    }
 }
 
 
@@ -37,8 +44,8 @@ void loop () {
  * @param httpsFingerprint String
  */
 void wifiStatus( void ) {
-	int result = 0;
-	result = WiFi.status( );
+    int result = 0;
+    result = WiFi.status( );
     s.sendAck( "i" , result );
 }
 
@@ -47,7 +54,7 @@ void wifiStatus( void ) {
  * Disconnect Wifi
  */
 void wifiDisconnect( void ) {
-	WiFi.disconnect( );
+    WiFi.disconnect( );
 }
 
 
@@ -55,9 +62,9 @@ void wifiDisconnect( void ) {
  * Connect to the Wifi network
  */
 void wifiBegin( void ) {
-	char ssid[33] = "";
-	char pass[20] = "";
-	s.getData( "ss" , &ssid , sizeof( ssid ) , &pass , sizeof( pass ) );
+    char ssid[33] = "";
+    char pass[20] = "";
+    s.getData( "ss" , &ssid , sizeof( ssid ) , &pass , sizeof( pass ) );
     s.sendAck( "i" , WiFi.begin( ssid , pass ) );
 }
 
@@ -66,10 +73,10 @@ void wifiBegin( void ) {
  * Return the current SSID
  */
 void wifiSSID( void ) {
-	char ssid[33] = "";
-	String Str_ssid;
-	Str_ssid = WiFi.SSID( );
-	Str_ssid.toCharArray( ssid , sizeof( ssid ) );
+    char ssid[33] = "";
+    String Str_ssid;
+    Str_ssid = WiFi.SSID( );
+    Str_ssid.toCharArray( ssid , sizeof( ssid ) );
     s.sendAck( "s" , ssid );
 }
 
@@ -78,8 +85,8 @@ void wifiSSID( void ) {
  * Return the MAC address of the router
  */
 void wifiBSSID( void ) {
-	uint8_t * bssid;
-	bssid = WiFi.BSSID();
+    uint8_t * bssid;
+    bssid = WiFi.BSSID();
     s.sendAck( "iiiiii" , bssid[0] , bssid[1] , bssid[2] , bssid[3]  , bssid[4] , bssid[5]  );
 }
 
@@ -88,8 +95,8 @@ void wifiBSSID( void ) {
  * Return the MAC address of the ESP8266 module
  */
 void wifiMAC( void ) {
-	byte mac[6];
-	WiFi.macAddress(mac);
+    byte mac[6];
+    WiFi.macAddress(mac);
     s.sendAck( "iiiiii" , mac[0] , mac[1] , mac[2] , mac[3]  , mac[4] , mac[5]  );
 }
 
@@ -98,11 +105,9 @@ void wifiMAC( void ) {
  * Return the IP address of the ESP8266 module
  */
 void wifiIP( void ) {
-	IPAddress ip_adr;
-	ip_adr = WiFi.localIP( );
+    IPAddress ip_adr;
+    ip_adr = WiFi.localIP( );
     s.sendAck( "iiii" , ip_adr[0], ip_adr[1] , ip_adr[2], ip_adr[3] );
-
-
 }
 
 
@@ -110,7 +115,46 @@ void wifiIP( void ) {
  * Return the IP address of the ESP8266 module
  */
 void wifiSubnet( void ) {
-	IPAddress subnet_adr;
-	subnet_adr = WiFi.subnetMask( );
+    IPAddress subnet_adr;
+    subnet_adr = WiFi.subnetMask( );
     s.sendAck( "iiii" , subnet_adr[0], subnet_adr[1] , subnet_adr[2], subnet_adr[3] );
 }
+
+
+/**
+ * Handle incoming HTTP request
+ */
+void handleHTTPRequest( void ) {
+    char tmpstr[50];
+    s.sendHeader( A_HTTPREQUEST );
+    httpserver->uri( ).toCharArray( tmpstr , sizeof( tmpstr ) );
+    s.sendcharArray( tmpstr );
+    s.sendInteger( httpserver->args() );
+    for (uint8_t i=0; i<httpserver->args(); i++){
+        httpserver->arg(i).toCharArray( tmpstr , sizeof( tmpstr ) );
+        s.sendcharArray( tmpstr );
+    }
+    s.sendFooter( );
+    httpserver->send( 200 , "text/plain", "a");
+}
+
+
+/**
+ * Start HTTP server
+ */
+void startHTTPServer( void ) {
+    if ( httpserverstarted )
+        return;
+    int port = 0;
+    s.getData( "i" , &port );
+    httpserver = new ESP8266WebServer( 80 );
+    httpserver->onNotFound( handleHTTPRequest );
+    httpserver->begin();
+    httpserverstarted = true;
+}
+
+
+
+
+
+
